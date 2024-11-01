378af716c0dbe4654b40664f275033085681582f
Contrast001 <1318116030@qq.com>
2024-11-01 22:07:07 +0800

    Set up basic Application and Entry Point.

---
+++ Aurora/src/Aurora.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//For use by Hazel applications
+#include "Aurora/Application.h"
+
+//------Entry Point--------
+#include"Aurora/EntryPoint.h"
+//-------------------------
---
+++ Aurora/src/Aurora/EntryPoint.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef AR_PLATFORM_WINDOWS
+extern Aurora::Application* Aurora::CreateApplication();
+
+int main(int argc,char** argv)
+{
+	auto app = Aurora::CreateApplication();
+	app->Run();
+	delete app;
+}
+#endif
