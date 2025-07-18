639154b7c2469640c2a9a524e03f965ac2fd88af
Contrast001 <1318116030@qq.com>
2025-07-19 00:20:40 +0800

    纹理批处理/62

--- Aurora/src/Aurora/Renderer/Renderer2D.cpp
+++ Aurora/src/Aurora/Renderer/Renderer2D.cpp
@@ -14,6 +14,7 @@ namespace Aurora{
 		glm::vec3 Position;
 		glm::vec4 Color;
 		glm::vec2 TexCoord;
+		float TexIndex;
 	};
 
 	struct Renderer2DData
@@ -21,7 +22,7 @@ namespace Aurora{
 		const uint32_t MaxQuads = 10000;
 		const uint32_t MaxVertices = MaxQuads * 4;
 		const uint32_t MaxIndices = MaxQuads * 6;
-
+		static const uint32_t MaxTextureSlots = 32;// 一次性的渲染上限
 
 		Ref<VertexArray> QuadVertexArray;
 		Ref<VertexBuffer> QuadVertexBuffer;
@@ -31,6 +32,9 @@ namespace Aurora{
 		uint32_t QuadIndexCount = 0;
 		QuadVertex* QuadVertexBufferBase = nullptr;
 		QuadVertex* QuadVertexBufferPtr = nullptr;
+
+		std::array<Ref<Texture2D>, MaxTextureSlots> TextureSlots;//纹理槽
+		uint32_t TextureSlotIndex = 1; // 0 为白色纹理
 	};
 
 	static Renderer2DData s_Data; 
@@ -48,7 +52,10 @@ namespace Aurora{
 		s_Data.QuadVertexBuffer->SetLayout({
 			{ShaderDataType::Float3,"a_Position"},
 			{ShaderDataType::Float4,"a_Color"},
-			{ ShaderDataType::Float2,"a_TexCoord" }
+			{ ShaderDataType::Float2,"a_TexCoord" },
+			{ ShaderDataType::Float,"a_TexIndex" },
+			//{ ShaderDataType::Float,"a_TilingFactor" }
+
 
 		});
 
@@ -80,10 +87,17 @@ namespace Aurora{
 		s_Data.WhiteTexture = Texture2D::Create(1, 1);
 		uint32_t whiteTextureData = 0xffffffff;
 		s_Data.WhiteTexture->SetData(&whiteTextureData,sizeof(uint32_t));
+
+		int32_t samplers[s_Data.MaxTextureSlots];
+		for (uint32_t i=0;i<s_Data.MaxTextureSlots;i++) 
+		{
+			samplers[i] = i;
+		}
 		s_Data.TextureShader = Shader::Create("assets/shaders/Texture.glsl");
 		s_Data.TextureShader->Bind();
-		s_Data.TextureShader->SetInt("u_Texture",0);
+		s_Data.TextureShader->SetIntArray("u_Textures",samplers,s_Data.MaxTextureSlots);
 
+		s_Data.TextureSlots[0] = s_Data.WhiteTexture;
 	}
 	void Renderer2D::Shutdown()
 	{
@@ -96,6 +110,7 @@ namespace Aurora{
 
 		s_Data.QuadIndexCount = 0;
 		s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
 		
 	}
 	void Renderer2D::EndScene()
@@ -106,6 +121,10 @@ namespace Aurora{
 	}
 	void Renderer2D::Flush()
 	{
+		for (uint32_t i=0;i<s_Data.TextureSlotIndex;i++) 
+		{
+			s_Data.TextureSlots[i]->Bind(i);
+		}
 		RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
 	}
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -114,25 +133,30 @@ namespace Aurora{
 	}
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
+		const float textureIndex = 0.0f; // 白色纹理的索引为0
 
 		s_Data.QuadVertexBufferPtr->Position = position;
 		s_Data.QuadVertexBufferPtr->Color = color;
 		s_Data.QuadVertexBufferPtr->TexCoord = {0.0f,0.0f};
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadVertexBufferPtr->Position = { position.x + size.x,position.y,0.0f };
 		s_Data.QuadVertexBufferPtr->Color = color;
 		s_Data.QuadVertexBufferPtr->TexCoord = { 1.0f,0.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadVertexBufferPtr->Position = { position.x + size.x,position.y + size.y,0.0f };;
 		s_Data.QuadVertexBufferPtr->Color = color;
 		s_Data.QuadVertexBufferPtr->TexCoord = { 1.0f,1.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadVertexBufferPtr->Position = { position.x,position.y+size.y,0.0f };;
 		s_Data.QuadVertexBufferPtr->Color = color;
 		s_Data.QuadVertexBufferPtr->TexCoord = { 0.0f,1.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
@@ -153,8 +177,52 @@ namespace Aurora{
 	}
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
 	{
-		s_Data.TextureShader->Bind();
+		constexpr glm::vec4 color = { 1.0f,1.0f,1.0f,1.0f };
 
+		float textureIndex = 0.0f;	
+
+		for(uint32_t i=1;i<s_Data.TextureSlotIndex;i++)
+		{
+			if (*s_Data.TextureSlots[i].get() == *texture.get())
+			{
+				textureIndex = (float)i;
+				break;
+			}
+		}
+
+		if(textureIndex==0.0f)
+		{
+			textureIndex = (float)s_Data.TextureSlotIndex;
+			s_Data.TextureSlots[s_Data.TextureSlotIndex] = texture;
+			s_Data.TextureSlotIndex++;
+		}
+
+		s_Data.QuadVertexBufferPtr->Position = position;
+		s_Data.QuadVertexBufferPtr->Color = color;
+		s_Data.QuadVertexBufferPtr->TexCoord = { 0.0f,0.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_Data.QuadVertexBufferPtr++;
+
+		s_Data.QuadVertexBufferPtr->Position = { position.x + size.x,position.y,0.0f };
+		s_Data.QuadVertexBufferPtr->Color = color;
+		s_Data.QuadVertexBufferPtr->TexCoord = { 1.0f,0.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_Data.QuadVertexBufferPtr++;
+
+		s_Data.QuadVertexBufferPtr->Position = { position.x + size.x,position.y + size.y,0.0f };;
+		s_Data.QuadVertexBufferPtr->Color = color;
+		s_Data.QuadVertexBufferPtr->TexCoord = { 1.0f,1.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_Data.QuadVertexBufferPtr++;
+
+		s_Data.QuadVertexBufferPtr->Position = { position.x,position.y + size.y,0.0f };;
+		s_Data.QuadVertexBufferPtr->Color = color;
+		s_Data.QuadVertexBufferPtr->TexCoord = { 0.0f,1.0f };
+		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_Data.QuadVertexBufferPtr++;
+
+		s_Data.QuadIndexCount += 6;
+#if OLD_PATH
 		s_Data.TextureShader->SetFloat4("u_Color", tintColor);
 		s_Data.TextureShader->SetFloat("u_TilingFactor",tilingFactor);
 		texture->Bind();
@@ -165,6 +233,7 @@ namespace Aurora{
 
 		s_Data.QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data.QuadVertexArray);
+#endif
 	}
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
--- Aurora/src/Platform/OpenGL/OpenGLShader.cpp
+++ Aurora/src/Platform/OpenGL/OpenGLShader.cpp
@@ -174,6 +174,12 @@ namespace Aurora {
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		UploadUniformIntArray(name, values,count);
+
+	}
+
 	void OpenGLShader::SetFloat(const std::string& name, float value)
 	{
 		UploadUniformFloat(name, value);
@@ -200,6 +206,12 @@ namespace Aurora {
 	{
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location,count, values);
 	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
