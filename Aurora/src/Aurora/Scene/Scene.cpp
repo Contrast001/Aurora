9dc34b261dc6ed0a29360aab54a1b3f56dfc3832
Contrast001 <1318116030@qq.com>
2026-02-25 23:39:37 +0800

    EnTT测试/77

--- Aurora/src/Aurora/Scene/Scene.cpp
+++ Aurora/src/Aurora/Scene/Scene.cpp
@@ -1,7 +1,53 @@
 #include "arpch.h"
 #include "Scene.h"
 
+#include <glm/glm.hpp>
 namespace Aurora{
 
+	static void DoMath(const glm::mat4& transform)
+	{
+	}
 
+	static void OnTransformConstruct(entt::registry& registry,entt::entity entity)
+	{
+	}
+	Scene::Scene()
+	{
+		struct TransformComponent 
+		{
+			glm::mat4 Transform;
+			TransformComponent() = default;
+			TransformComponent(const TransformComponent&) = default;
+			TransformComponent(const glm::mat4& transform)
+				:Transform(transform) {}
+			operator  glm::mat4& ()  { return Transform; }
+			operator const glm::mat4& () const { return Transform; }
+		};
+
+		struct MeshComponent {};
+
+
+
+		entt::entity entity = m_Registry.create();
+		m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
+
+		m_Registry.on_construct<TransformComponent>().connect<&OnTransformConstruct>();
+
+		auto view = m_Registry.view<TransformComponent>();
+		for(auto entity:view)
+		{
+			TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
+		}
+
+		auto group = m_Registry.group<TransformComponent>(entt::get<MeshComponent>);
+		/*for(auto entity :group)
+		{
+			auto&[transform, mesh] = group.get<TransformComponent, MeshComponent>(entity);
+		}*/
+
+	}
+
+	Scene::~Scene()
+	{
+	}
 }
